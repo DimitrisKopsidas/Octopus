3f18d316be8cbe3b82df29c083504e32ecc7b847
Aggelos Kampas <aggelos.cambas@gmail.com>
2026-07-02 23:32:38 +0300

    Prototype Exercise class

---
+++ backend/src/main/java/com/dkopsidas/octopus/service/Prototype/Engine.java
@@ -0,0 +1,16 @@
+package com.dkopsidas.octopus.service.Prototype;
+
+import net.objecthunter.exp4j.Expression;
+import net.objecthunter.exp4j.ExpressionBuilder;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Engine {
+
+    public static void main(String[] args){
+
+
+    }
+}
---
+++ backend/src/main/java/com/dkopsidas/octopus/service/Prototype/Exercise.java
@@ -0,0 +1,50 @@
+package com.dkopsidas.octopus.service.Prototype;
+
+import java.util.List;
+import java.util.Map;
+
+public class Exercise {
+
+    private String title;
+    private String promnt;
+    private String imageUrl;
+
+    private Map<String, Double> variables;
+
+    private List<String> goals;
+
+    private List<String> steps;
+
+    public Exercise(String promnt, String title, String imageUrl, Map<String, Double> variables, List<String> goals, List<String> steps) {
+        this.promnt = promnt;
+        this.title = title;
+        this.imageUrl = imageUrl;
+        this.variables = variables;
+        this.goals = goals;
+        this.steps = steps;
+    }
+
+    public String getPromnt() {
+        return promnt;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public Map<String, Double> getVariables() {
+        return variables;
+    }
+
+    public List<String> getGoals() {
+        return goals;
+    }
+
+    public List<String> getSteps() {
+        return steps;
+    }
+}
