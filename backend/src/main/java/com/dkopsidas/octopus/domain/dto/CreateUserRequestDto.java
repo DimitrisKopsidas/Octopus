c4d5d2364c6acec60f388058f80f6d90a7295e07
DimitrisKopsidas <jimkopsidas@hotmail.com>
2026-08-25 17:39:19 +0300

    fix(user): change year limits to represent year of enrollment and added discord name to update dto

--- backend/src/main/java/com/dkopsidas/octopus/domain/dto/CreateUserRequestDto.java
+++ backend/src/main/java/com/dkopsidas/octopus/domain/dto/CreateUserRequestDto.java
@@ -16,10 +16,15 @@ public record CreateUserRequestDto(
         @Size(max = 100, message = ERROR_MESSAGE_PASSWORD_SIZE)
         String password,
 
+        @NotBlank(message = ERROR_MESSAGE_YEAR_REQUIRED)
+        @Size(min = 2000, max = 2030, message = ERROR_MESSAGE_YEAR_LIMIT)
         Integer year,
 
         @Size(max = 50, message = ERROR_MESSAGE_USER_CODE_SIZE)
-        String userCode
+        String userCode,
+
+        @Size(max = 255, message = ERROR_MESSAGE_DISCORD_NAME_LIMIT)
+        String discordName
 ) {
     private static final String ERROR_MESSAGE_USERNAME_REQUIRED = "Username is required";
     private static final String ERROR_MESSAGE_USERNAME_SIZE = "Username must be 50 characters or less";
@@ -28,4 +33,7 @@ public record CreateUserRequestDto(
     private static final String ERROR_MESSAGE_PASSWORD_REQUIRED = "Password is required";
     private static final String ERROR_MESSAGE_PASSWORD_SIZE = "Password must be 100 characters or less";
     private static final String ERROR_MESSAGE_USER_CODE_SIZE = "User code must be 50 characters or less";
+    private static final String ERROR_MESSAGE_YEAR_REQUIRED = "Display name is required";
+    private static final String ERROR_MESSAGE_YEAR_LIMIT = "Year must be between 2000 and 2030";
+    private static final String ERROR_MESSAGE_DISCORD_NAME_LIMIT = "Discord name must be less than 255 characters";
 }
--- backend/src/main/java/com/dkopsidas/octopus/domain/dto/UpdateUserYearRequestDto.java
+++ backend/src/main/java/com/dkopsidas/octopus/domain/dto/UpdateUserYearRequestDto.java
@@ -6,7 +6,7 @@ import jakarta.validation.constraints.NotNull;
 
 public record UpdateUserYearRequestDto(
         @NotNull(message = "Year is required")
-        @Min(value = 1, message = "Year must be at least 1")
-        @Max(value = 5, message = "Year cannot exceed 5")
+        @Min(value = 2000, message = "Year must be at least 2000")
+        @Max(value = 2030, message = "Year cannot exceed 2030")
         Integer year
 ) {}
